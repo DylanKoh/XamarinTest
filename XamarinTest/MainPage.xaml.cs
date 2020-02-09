27dec84a467392176197329d8c3386757fc3f4bd
KOH WEI LONG DYLAN TP <1907446G@student.tp.edu.sg>
2020-02-09 22:12:11 +0800

    Add project files.

---
+++ XamarinTest/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XamarinTest
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private async void LoginButton_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new LoginPage());
+        }
+
+        private async void SignUp_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new LoginPage());
+        }
+    }
+}
---
+++ XamarinTest/Model/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+
+namespace XamarinTest.Model
+{
+    public class User
+    {
+        [PrimaryKey, AutoIncrement]
+        public int userID { get; set; }
+        public string username { get; set; }
+        public string password { get; set; }
+    }
+}
